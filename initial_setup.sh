7944670ca281d8993a7761897fabec459d9cb237
Gonçalo Ribeiro <gribeiro@ua.pt>
2024-08-28 01:24:53 +0100

    update

--- initial_setup.sh
+++ initial_setup.sh
@@ -12,3 +12,12 @@ ln -sf "$HOME/linux_configs/.config/hypr" "$HOME/.config"
 # rofi
 rm -rf $HOME/.config/rofi
 ln -sf "$HOME/linux_configs/.config/rofi" "$HOME/.config"
+# Waybar
+rm -rf $HOME/.config/waybar
+ln -sf "$HOME/linux_configs/.config/waybar" "$HOME/.config"
+# Terminator
+rm -rf $HOME/.config/terminator
+ln -sf "$HOME/linux_configs/.config/terminator" "$HOME/.config"
+# SDDM
+sudo find "$HOME/linux_configs/login_manager/sddm/themes/" -mindepth 1 -maxdepth 1 -exec ln -sf {} "/usr/share/sddm/themes" \;
+sudo ln -sf "$HOME/linux_configs/login_manager/sddm/sddm.conf" "/etc/sddm.conf"
